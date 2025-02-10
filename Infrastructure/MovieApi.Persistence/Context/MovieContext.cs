2c4baa167f9ef8336e1570c1c52889566b28fe2a
Mahmut Can Sarıbal <mahmutcansaribal_19@hotmail.com>
2025-02-10 14:39:23 +0300

    Started MovieApi Project

---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MAHMUTCAN;initial Catalog=ApiMoiveDb;integrated Security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
+
+//TODO Domain katmanımıza, diğer katmanlar erişim sağlayabilir. Persistance katmanı domain katmanına erişim sağlayacak ki, veri tabanına yansıtılacak tablolarımızı çağırabilelim
